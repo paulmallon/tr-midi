125c5f62d041a61ad9a2ffa8f7585190794f793f
Paul Mallon <pm@compost.se>
2017-03-26 01:01:53 +0100

    rpt midi stuff

--- src/main/java/com/nullterrier/TrMidiApplication.java
+++ src/main/java/com/nullterrier/TrMidiApplication.java
@@ -4,6 +4,7 @@ import com.ecyrd.speed4j.StopWatch;
 import com.ecyrd.speed4j.StopWatchFactory;
 import com.nullterrier.service.DefaultMidiService;
 import com.nullterrier.service.MidiService;
+import de.humatic.nmj.NetworkMidiSystem;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -21,6 +22,8 @@ public class TrMidiApplication implements CommandLineRunner {
     @Value("${dummy:true}")
     private boolean dummy;
 
+    public NetworkMidiSystem networkMidiSystem;
+
     private static final Logger log = LoggerFactory.getLogger(DefaultMidiService.class);
 
     public static void main(String[] args) {
@@ -34,6 +37,12 @@ public class TrMidiApplication implements CommandLineRunner {
 
     @Override
     public void run(String... strings) throws InterruptedException {
+        NetworkMidiSystem networkMidiSystem = NetworkMidiSystem.get();
+        try {
+            networkMidiSystem.openInput(1, null);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
 
         midiService.init();
         midiService.listAllMidiDevices();
